29994e0ae84f14d78878021903c758494ed5914c
zMag33z <mag33.cdh@gmail.com>
2023-05-04 20:24:35 -0500

    utils folder and formulations added

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from app.db import init_db
 from app.routes import home, dashboard
 
 def create_app(test_config=None):
@@ -18,5 +18,7 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  init_db(app)
 
   return app
